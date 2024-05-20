9d900e8c20a045beeb6b42fd48a4310e5dce2406
이재하 <jhl2619@naver.com>
2024-05-20 22:58:19 +0900

    chore: Add black colors

--- src/lib/styles/theme.ts
+++ src/lib/styles/theme.ts
@@ -2,6 +2,10 @@ const colors = {
   main: "#7761FF",
   border_01: "#D6DDFF",
   border_02: "#D9D9D9",
+  black_01: "#222222",
+  black_02: "#666666",
+  black_03: "#9E9E9E",
+  black_04: "#EEEEEE",
 } as const;
 
 export type ThemeColors = typeof colors;
