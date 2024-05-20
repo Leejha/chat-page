920abfd2a63bd0c5c8f1af19e2955108d46aa3e9
이재하 <jhl2619@naver.com>
2024-05-20 20:34:02 +0900

    feat: Add useLogin service

---
+++ src/services/use-login.ts
@@ -0,0 +1,32 @@
+import { useForm, useLoginValidation } from "../hooks";
+
+import { LoginRequest, PATH, loginAPI, userStorage } from "../lib";
+
+export default function useLogin() {
+  const { formData, onChangeForm } = useForm<LoginRequest>({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const { onFormValidation } = useLoginValidation();
+
+  const onLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (onFormValidation(email, password)) {
+      try {
+        const response = await loginAPI(formData);
+        userStorage.set(response);
+
+        window.location.replace(PATH.CHAT_PAGE);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          alert(`에러가 발생했습니다. ${error.message}`);
+        }
+      }
+    }
+  };
+
+  return { onLogin, formData, onChangeForm };
+}
