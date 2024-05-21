685fc03e19abbd6cdf830743d9c1818b3109c1ad
이재하 <jhl2619@naver.com>
2024-05-21 17:10:07 +0900

    feat: Add publishing chat page

--- src/pages/chat-page.tsx
+++ src/pages/chat-page.tsx
@@ -1,9 +1,108 @@
+import styled from "styled-components";
+import { Button, MessageUser } from "../components";
+import MessageSangnyang from "../components/message-sangnyang";
+
 function ChatPage() {
   return (
-    <div>
-      <h1>Chat Page</h1>
-    </div>
+    <Layout>
+      <Content>
+        <Header>
+          <img src="src/assets/menu-icon.svg" alt="menu" />
+          <img src="src/assets/new-icon.svg" alt="new" />
+        </Header>
+        <ChatContainer>
+          <DateBox>2023년 41월 12일</DateBox>
+          <MessageList>
+            <MessageSangnyang isFirstMessage />
+            <MessageSangnyang />
+            <MessageUser />
+          </MessageList>
+          <InputSection>
+            <Input placeholder="상냥이와 이야기를 나눠보세요!" />
+            <InputButton width="32px" height="32px"></InputButton>
+          </InputSection>
+        </ChatContainer>
+      </Content>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 480px;
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
+  box-shadow: 0px -1px 0px 0px ${({ theme }) => theme.colors.black_04} inset;
+`;
+
+const DateBox = styled.div`
+  width: 104px;
+  height: 24px;
+  padding: 4px 0;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.colors.black_04};
+  margin: 28px auto 0 auto;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 17px;
+  letter-spacing: -0.04em;
+  text-align: center;
+`;
+
+const ChatContainer = styled.main`
+  position: relative;
+  height: calc(100vh - 50px);
+  display: flex;
+  flex-direction: column;
+`;
+
+const MessageList = styled.ul`
+  flex: 1;
+  overflow: auto;
+  margin-top: 24px;
+`;
+
+const InputSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.black_04};
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 56px;
+  padding: 0 16px;
+`;
+
+const Input = styled.input`
+  border-radius: 8px;
+  width: 400px;
+  height: 40px;
+  padding: 8px;
+  background-color: white;
+  font-weight: 500;
+  letter-spacing: -0.02em;
+  color: ${({ theme }) => theme.colors.black_01};
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.black_03};
+  }
+`;
+
+const InputButton = styled(Button)`
+  margin-left: 16px;
+  background: url("src/assets/send-icon.svg")
+    ${({ theme }) => theme.colors.main} no-repeat center;
+`;
 
 export default ChatPage;
