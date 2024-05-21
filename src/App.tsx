ed56cea04c21c56886b1034f1c15806dbae03c2a
이재하 <jhl2619@naver.com>
2024-05-22 02:50:39 +0900

    feat: Add react-query QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Outlet } from "react-router-dom";
 
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, theme } from "./lib/styles";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const [reactQueryClient] = useState(() => new QueryClient());
   return (
-    <ThemeProvider theme={theme}>
-      <Outlet />
-      <GlobalStyles />
-    </ThemeProvider>
+    <QueryClientProvider client={reactQueryClient}>
+      <ThemeProvider theme={theme}>
+        <Outlet />
+        <GlobalStyles />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
