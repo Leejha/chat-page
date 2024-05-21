07c8c2eec899cdb2c7262f8e9be10f9f599abc47
이재하 <jhl2619@naver.com>
2024-05-21 22:47:56 +0900

    feat: Add useOutsideClick hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { default as useForm } from "./use-form.ts";
 export { default as useLoginValidation } from "./use-login-validation.ts";
+export { default as useToggle } from "./use-toggle.ts";
+export { default as useOutsideClick } from "./use-outside-click.ts";
---
+++ src/hooks/use-outside-click.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export default function useOutsideClick<T extends HTMLElement>(
+  isOpen: boolean,
+  onClose: () => void
+) {
+  const targetEl = useRef<T>(null);
+
+  const onClickOutSide = useCallback(
+    (e: MouseEvent) => {
+      const { target } = e;
+      if (target instanceof Node) {
+        if (isOpen && !targetEl.current?.contains(target)) {
+          onClose();
+        }
+      }
+    },
+    [isOpen, onClose]
+  );
+
+  useEffect(() => {
+    window.addEventListener("click", onClickOutSide);
+    return () => {
+      window.removeEventListener("click", onClickOutSide);
+    };
+  }, [onClickOutSide]);
+
+  return {
+    targetEl,
+  };
+}
