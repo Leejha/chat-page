d503a05ad5259f6b7b8193d89fba2a6c36c43bc7
이재하 <jhl2619@naver.com>
2024-05-22 02:58:37 +0900

    feat: Add Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+interface Props {
+  onResetCurrentChatList: () => void;
+}
+
+function Header({ onResetCurrentChatList }: Props) {
+  return (
+    <Container>
+      <img src="src/assets/menu-icon.svg" alt="menu" />
+      <Button onClick={onResetCurrentChatList}>
+        <img src="src/assets/new-icon.svg" alt="new" />
+      </Button>
+    </Container>
+  );
+}
+
+const Container = styled.header`
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
+  box-shadow: 0px -1px 0px 0px ${({ theme }) => theme.colors.black_04} inset;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+`;
+
+export default Header;
